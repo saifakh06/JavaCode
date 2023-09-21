350e54ba51405a0c6df7136e25d818a2da699c43
saifakh06 <samimmd949@gmail.com>
2023-09-21 11:00:36 +0530

    "Added a program called FirstNnumber

---
+++ JavaProject/src/practicePrograms/FirstNnumbers.java
@@ -0,0 +1,14 @@
+package practicePrograms;
+
+public class FirstNnumbers {
+	public static void main(String args []) {
+		int n = 10;
+	     int sum =0;
+
+	     for (int i=1; i<=n; i++) {
+	         sum += i;
+	       
+		} System.out.println (sum);
+	}
+
+}
