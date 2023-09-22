ca8250500161a945e2b1914587b433106045fc6f
saifakh06 <samimmd949@gmail.com>
2023-09-22 18:20:28 +0530

    Added two java programs

---
+++ JavaProject/src/practicePrograms/SumInRange.java
@@ -0,0 +1,15 @@
+package practicePrograms;
+
+public class SumInRange {
+	public static void main(String args[]) {
+		
+		int intialValue = 5;
+		int finalValue = 15;
+		int sum = 0;
+		
+		for (int i = 5;i<=15;i++) {
+			sum = sum +i;
+		} System.out.println(sum);
+	}
+
+}
---
+++ JavaProject/src/practicePrograms/SumOfDigitOfNumber.java
@@ -0,0 +1,16 @@
+package practicePrograms;
+
+public class SumOfDigitOfNumber {
+
+	public static void main(String[] args) {
+		
+		int num = 567;
+		int sum = 0;
+		while (num!=0) {
+			sum = sum + num%10;
+			num = num/10;
+		} System.out.println(sum);
+
+	}
+
+}
